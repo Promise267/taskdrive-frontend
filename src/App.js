8959e96924c077426b974feaa79a50b9dd61117a
Promise Rijal <promrizal@gmail.com>
2023-07-13 15:00:50 +0545

    project-completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./components/Home/Home.jsx"
+import Note from './components/Note/Note';
+import About from './components/About';
+import AddNote from './components/Note/AddNote'
 
 function App() {
+
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path='/about' element ={<About/>}/>
+      <Route path="/collection/:id" element = {<Note/>}/>
+      <Route path="/collection/:id/newnote" element = {<AddNote/>}/>
+    </Routes>
+    
   );
 }
 
